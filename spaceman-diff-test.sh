d80fe8b90a849b7331921735b9693256c378a7f8
Zach Holman <zach@zachholman.com>
2015-07-09 18:58:45 -0500

    add roundup and round up some tests

---
+++ spaceman-diff-test.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env roundup
+
+describe "spaceman-diff: generates ascii diffs!"
+
+spaceman="./spaceman-diff"
+
+it_shows_help_with_no_argv() {
+  $spaceman | grep USAGE
+}
+
+it_falls_back_for_non_image_data() {
+  output="$spaceman 5461eb0cf2c766799e9b5325b9b9eb54a0af0f7a"
+
+  $output | grep "diff --git a/spaceman-diff b/spaceman-diff"
+}
