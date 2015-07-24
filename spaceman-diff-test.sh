374838406361558bf28573c5cdfa888334ac04b2
Zach Holman <zach@zachholman.com>
2015-07-23 17:23:44 -0700

    Don't worry about this as much now that we have gitattributes

--- spaceman-diff-test.sh
+++ spaceman-diff-test.sh
@@ -8,18 +8,6 @@ it_shows_help_with_no_argv() {
   $spaceman | grep USAGE
 }
 
-it_falls_back_for_non_image_data() {
-  output="$spaceman 5461eb0cf2c766799e9b5325b9b9eb54a0af0f7a"
-
-  $output | grep "diff --git a/spaceman-diff b/spaceman-diff"
-}
-
-it_deals_with_directories() {
-  output="$spaceman test deadbeef 100644 test /dev/null 100644"
-
-  $output | grep "no img"
-}
-
 it_deals_with_new_image() {
   output="$spaceman an-image-tho.png deadbeef 100644 img-tho.png /dev/null 100644"
 
