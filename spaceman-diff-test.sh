e64feb4f98dd96981d6aba5b4452f0bf611b3eb3
Mark Przepiora <mark.przepiora@gmail.com>
2016-01-11 11:11:18 -0700

    Speed up test suite

--- spaceman-diff-test.sh
+++ spaceman-diff-test.sh
@@ -4,25 +4,33 @@ describe "spaceman-diff: generates ascii image diffs!"
 
 spaceman="./spaceman-diff"
 
+run_spaceman() {
+  readonly output_file=$(mktemp /tmp/XXXXX)
+  $spaceman "$@" > "$output_file"
+  echo "$output_file"
+}
+
 it_shows_help_with_no_argv() {
   $spaceman | grep USAGE
 }
 
 it_renders_diff() {
-  output="$spaceman test/images/flag.png test/images/flag.png a190ba 100644 test/images/gooder-flag.png 000000 100644"
+  output_file=$(run_spaceman \
+    test/images/flag.png \
+    test/images/flag.png a190ba 100644 \
+    test/images/gooder-flag.png 000000 100644)
 
-  $output | grep "OLD:"
-  $output | grep "(84 KB)"
-  $output | grep "NEW:"
-  $output | grep "(9 KB)"
+  grep -F "OLD: flag.png (84 KB)" < "$output_file"
+  grep -F "NEW: gooder-flag.png (9 KB)" < "$output_file"
+
+  rm "$output_file"
 }
 
 it_works_with_output_filenames_containing_spaces() {
-  output_file=$(mktemp /tmp/XXXXX)
-
-  $spaceman "test/images/flag.png" \
+  output_file=$(run_spaceman \
+    "test/images/flag.png" \
     "test/images/flag.png" a190ba 100644 \
-    "test/images/with spaces.png" 000000 100644 > "$output_file"
+    "test/images/with spaces.png" 000000 100644)
 
   grep -F 'OLD: flag.png (84 KB)' < "$output_file"
   grep -F 'NEW: with spaces.png (9 KB)' < "$output_file"
@@ -31,11 +39,10 @@ it_works_with_output_filenames_containing_spaces() {
 }
 
 it_works_with_input_filenames_containing_spaces() {
-  output_file=$(mktemp /tmp/XXXXX)
-
-  $spaceman "test/images/with spaces.png" \
+  output_file=$(run_spaceman \
+    "test/images/with spaces.png" \
     "test/images/with spaces.png" a190ba 100644 \
-    "test/images/flag.png" 000000 100644 > "$output_file"
+    "test/images/flag.png" 000000 100644)
 
   grep -F 'OLD: with spaces.png (9 KB)' < "$output_file"
   grep -F 'NEW: flag.png (84 KB)' < "$output_file"
